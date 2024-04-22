040b08d03b3bfb08cc1aa1da63a02375a4fa5de4
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-22 23:50:00 +0530

    Update loading.tsx

--- app/ui/dashboard/loading.tsx
+++ app/ui/dashboard/loading.tsx
@@ -1,7 +1,8 @@
+import { CardSkeleton } from "../skeletons";
 export default function Loading() {
     return (
         <div>
-            Loading...
+            <CardSkeleton/>
         </div>
     );
 }
