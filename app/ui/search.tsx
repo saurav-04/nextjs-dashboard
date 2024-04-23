f7485510201701357bc5357c8331f94817cf9fd3
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-23 17:35:05 +0530

    Update search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams,usePathname,useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -9,7 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname()
   const {replace}= useRouter()
 
-  function handleSearch (term:string){
+  const handleSearch = useDebouncedCallback((term:string)=>{
     console.log(`Searching...${term}`);
     const params = new URLSearchParams(searchParams)
     if (term){
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  },1000)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
