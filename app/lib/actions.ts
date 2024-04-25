745ec42b77174f7c05ff3cd8e9142606d107ee82
Saurav Achari <123930348+saurav-04@users.noreply.github.com>
2024-04-25 18:30:38 +0530

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,7 +27,7 @@ export type State = {
   message?: string | null;
 };
 
-export async function createInvoice(prevState: State, formData: FormData) {
+export async function createInvoice( prevState: State, formData: FormData) {
     const validatedFields = CreateInvoice.safeParse({
         customerId : formData.get('customerId'),
         amount : formData.get('amount'),
@@ -41,7 +41,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       };
     }
 
-    console.log (validatedFields);
+    console.log(prevState)
 
     const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
@@ -67,29 +67,39 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 // ...
  
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+ 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
-try{
-  await sql`
-  UPDATE invoices
-  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-  WHERE id = ${id}
-`;
-}catch(error){
-  return{
-    message:'Database Error: Failed to Update Invoice.',
-  };
-};
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
  
-  revalidatePath('/ui/invoices');
-  redirect('/ui/invoices');
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
